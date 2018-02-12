a6d0bbd9b10bc1b3c40fc5c11026076befce505a
rettar <rettar.v@gmail.com>
2018-02-12 17:24:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Oruka
+## Install
+
+```
+$ cd ./ODN-CLIENT
+$ cd ./src/plugins/tweet
+$ git clone https://github.com/rettar5/OCPT_Oruka.git oruka
+$ npm run load:build
+```
---
+++ oruka.ts
@@ -0,0 +1,35 @@
+import { OdnTweetData, OdnTweets } from "../../../odnTweets"
+import { OdnPlugins, OdnPluginResultData } from "../../../odnPlugins";
+import {Log, OdnUtils} from "../../../odnUtils";
+
+export class Oruka {
+  constructor(private tweetData: OdnTweetData, private fullName: string) {}
+
+  /**
+   * プラグインのメイン処理を実行
+   *
+   * @param {(isProcessed?: boolean) => void} finish
+   */
+  run(finish: (isProcessed?: boolean) => void) {
+    const tweets = new OdnTweets(this.tweetData.accountData);
+    const kusa = "ｗ".repeat(OdnUtils.rand(1, 10));
+    tweets.text = "おらんな" + kusa;
+    tweets.targetTweetId = this.tweetData.idStr;
+
+    // ツイートを投稿
+    tweets.postTweet((isSuccess) => {
+      tweets.likeTweet();
+      finish();
+    });
+  }
+
+  /**
+   * プラグインを実行するかどうか判定
+   *
+   * @param {OdnTweetData} tweetData
+   * @returns {boolean}
+   */
+  static isValid(tweetData: OdnTweetData): boolean {
+    return tweetData.text.match(/^.*(おるか).*(w|ｗ)+$/gi) ? true : false;
+  }
+}
